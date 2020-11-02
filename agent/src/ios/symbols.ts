eb89f2c11a7e078b9f6b5340563617f8b6021b4d
3eed <hot3eed@gmail.com>
2020-11-02 16:21:35 +0200

    Fix _xpc_connection_call_event_handler string

--- agent/src/ios/symbols.ts
+++ agent/src/ios/symbols.ts
@@ -5,6 +5,6 @@ export namespace symbols {
         const p_xpc_data_set_value: NativePointer = Module.getExportByName(null, '_xpc_data_set_value');  // This is the nearest global symbol to the one we're hooking, as of iOS 13.6
         const p__xpc_connection_call_event_handler: NativePointer = p_xpc_data_set_value.sub(0xBFC);
 
-        return ['_xpc_data_set_value', p__xpc_connection_call_event_handler];
+        return ['_xpc_connection_call_event_handler', p__xpc_connection_call_event_handler];
     } 
 }
